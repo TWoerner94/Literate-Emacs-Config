e5fdd00b63e8c418688e1e1fbe7fb39e6d01f1e9
Tim Julian Wörner <tim.woerner@usd.de>
2021-12-27 19:38:44 +0100

    change org-babel order, commit init.el

---
+++ init.el
@@ -0,0 +1,15 @@
+(require 'org)
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(geiser-racket magit ox-hugo use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
