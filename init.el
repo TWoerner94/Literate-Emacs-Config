f5491a9dfe3a3bacd1afe9374e101e45bde9a6b4
Tim Julian Wörner <tim.woerner@usd.de>
2021-12-31 13:41:24 +0100

    add auctex config and some packages

--- init.el
+++ init.el
@@ -6,7 +6,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(auctex geiser-racket magit ox-hugo use-package)))
+ '(package-selected-packages
+   '(org-download htmlize counsel ivy auctex geiser-racket magit ox-hugo use-package))
+ '(vertico-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
